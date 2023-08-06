4714eb5aa8c9d8c9db0e88775be31e7431f6720f
fhawk12 <2369187058@qq.com>
2023-08-06 17:36:37 +0800

    dwm:modify default app in tab

--- dwm/config.h
+++ dwm/config.h
@@ -31,7 +31,7 @@ static const unsigned int alphas[][3]      = {
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4 ", "5", "6", "7", "8 ", "9 " };
-static const char *defaulttagapps[] = { "", NULL, NULL, "", NULL, NULL, NULL, "linuxqq", "Clash for Windows" };
+static const char *defaulttagapps[] = { "", NULL, NULL, "", NULL, NULL, NULL, "linuxqq", "cfw" };
 
 static const Rule rules[] = {
 	/* xprop(1):
